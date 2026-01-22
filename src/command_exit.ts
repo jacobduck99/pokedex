0a7adeea5b9334239c97e78d9b7ae8ddcda387ef
Jacob Duckworth <jacob.formaz@gmail.com>
2026-01-22 20:09:06 +1100

    Changing functions to async

--- src/command_exit.ts
+++ src/command_exit.ts
@@ -1,6 +1,6 @@
 import type { State } from "./state.js";
 
-export function commandExit(state: State) {
+export function commandExit(state: State): Promies<void> {
   console.log("Closing the Pokedex... Goodbye!");
   state.rl.close();
   process.exit(0);
