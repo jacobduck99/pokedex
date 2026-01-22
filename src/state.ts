c4b96032426c81f8f0da26710a1779a52b6ca689
Jacob Duckworth <jacob.formaz@gmail.com>
2026-01-22 18:07:22 +1100

    Updated state and async to now use the api

--- src/state.ts
+++ src/state.ts
@@ -5,25 +5,29 @@ import { PokeAPI } from "./pokeapi.js";
 export type State = {
     rl: Interface;
     commands: Record<string, CLICommand>;
-    pokedexApi: PokeAPI;
-    nextLocationsURL: string | null;
-    prevLocationsURL: string | null; 
+    pokeAPI: PokeAPI;
+    nextLocationsURL: string;
+    prevLocationsURL: string; 
 };
 
 export type CLICommand = {
   name: string;
   description: string;
-  callback: (state: State) => void;
+  callback: (state: State) => Promise<void>;
 };
 
 export function initState(): State {
-  const rl = createInterface({
+    const rl = createInterface({
     input: process.stdin,
     output: process.stdout,
-    prompt: " ",
-  });
+    prompt: "Pokedex > ",
+    });
 
-  const commands = getCommands();
+    const pokeAPI = new PokeAPI();
+    const nextLocationsURL = "";
+    const prevLocationsURL = "";
 
-  return { rl, commands };
+    const commands = getCommands();
+
+  return { rl, commands, pokeAPI, nextLocationsURL, prevLocationsURL };
 }
