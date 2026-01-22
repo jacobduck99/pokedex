8a69a7c30a8b2064bb7e92f660f37d3e6fc4800e
Jacob Duckworth <jacob.formaz@gmail.com>
2026-01-22 20:02:07 +1100

    handle api call in repl loop

--- src/command_map.ts
+++ src/command_map.ts
@@ -1,5 +1,5 @@
 import type { State } from "./state.js";
 
-async function commandMap(state: State): Promise<void> {
+export async function commandMap(state: State): Promise<void> {
     const locations = await state.pokeAPI.fetchLocations(); 
 };
