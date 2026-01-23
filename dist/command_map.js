1913962dd4701a3cd1df6ba04dc71f74f58471e7
Jacob Duckworth <jacob.formaz@gmail.com>
2026-01-23 16:12:31 +1100

    Added logic in map api to use next location

--- dist/command_map.js
+++ dist/command_map.js
@@ -1,5 +1,5 @@
 export async function commandMap(state) {
-    const locations = await state.pokeAPI.fetchLocations();
+    const locations = await state.pokeAPI.fetchLocations(state.nextLocationsURL);
     state.nextLocationsURL = locations.next;
     state.prevLocationsURL = locations.previous;
     for (const loc of locations.results) {
--- src/command_map.ts
+++ src/command_map.ts
@@ -1,10 +1,11 @@
 import type { State } from "./state.js";
 
 export async function commandMap(state: State): Promise<void> {
-  const locations = await state.pokeAPI.fetchLocations();
+    const locations = await state.pokeAPI.fetchLocations(state.nextLocationsURL);
 
-  state.nextLocationsURL = locations.next;
-  state.prevLocationsURL = locations.previous;
+    state.nextLocationsURL = locations.next;
+    state.prevLocationsURL = locations.previous;
+
 
   for (const loc of locations.results) {
     console.log(loc.name);
