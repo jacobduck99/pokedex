f45f491e9e093f2bc4ed36a9633eb4a8790449ab
Jacob Duckworth <jacob.formaz@gmail.com>
2026-01-24 13:00:51 +1100

    Wrote command explore to get pokemon from api

---
+++ src/command_explore.ts
@@ -0,0 +1,12 @@
+import type { State } from "./state.js";
+
+export async function commandExplore(state: State, ...args: string[]) {
+    let locationArea = args[0];
+    console.log(`Exploring ${locationArea}...`);
+    const location = await state.pokeAPI.fetchLocation(`${locationArea}`);
+    for (const pokemon of location.pokemon_encounters) {
+        console.log(`- ${pokemon.name}`);
+    } 
+    
+}
+
